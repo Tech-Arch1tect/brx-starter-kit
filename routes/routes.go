5c87c876f080daf96da7e64163281aef68097052
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-08-14 23:21:37 +0100

    add auth routes rate limiting

--- routes/routes.go
+++ routes/routes.go
@@ -2,25 +2,37 @@ package routes
 
 import (
 	"net/http"
+	"time"
 
 	"brx-starter-kit/handlers"
+
 	"github.com/labstack/echo/v4"
+	"github.com/tech-arch1tect/brx/config"
+	"github.com/tech-arch1tect/brx/middleware/ratelimit"
 	"github.com/tech-arch1tect/brx/server"
 	"github.com/tech-arch1tect/brx/session"
 )
 
-func RegisterRoutes(srv *server.Server, dashboardHandler *handlers.DashboardHandler, authHandler *handlers.AuthHandler, sessionManager *session.Manager) {
+func RegisterRoutes(srv *server.Server, dashboardHandler *handlers.DashboardHandler, authHandler *handlers.AuthHandler, sessionManager *session.Manager, rateLimitStore ratelimit.Store) {
 	e := srv.Echo()
 	e.Use(session.Middleware(sessionManager))
 
 	// Static file serving for Vite assets
 	srv.Get("/build/*", echo.WrapHandler(http.StripPrefix("/build/", http.FileServer(http.Dir("public/build")))))
 
-	// Authentication routes
+	// Authentication routes with rate limiting
+	authRateLimit := ratelimit.WithConfig(&ratelimit.Config{
+		Store:        rateLimitStore,
+		Rate:         5,
+		Period:       time.Minute,
+		CountMode:    config.CountFailures,
+		KeyGenerator: ratelimit.SecureKeyGenerator,
+	})
+
 	srv.Get("/login", authHandler.ShowLogin)
-	srv.Post("/login", authHandler.Login)
+	srv.Post("/login", authHandler.Login, authRateLimit)
 	srv.Get("/register", authHandler.ShowRegister)
-	srv.Post("/register", authHandler.Register)
+	srv.Post("/register", authHandler.Register, authRateLimit)
 	srv.Post("/logout", authHandler.Logout)
 	srv.Get("/profile", authHandler.Profile)
 
